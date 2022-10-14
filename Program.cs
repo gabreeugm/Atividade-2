88b77c4aff5e0e55d184c2fdf9eaff817fc14d59
Caio de Lima Pires <110869372+CaioDeLimaPires@users.noreply.github.com>
2022-10-13 23:22:37 -0300

    Finalizado minha parte caio :)))

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Atividade_2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Splash());
+            Application.Run(new Fornecedor());
         }
     }
 }
